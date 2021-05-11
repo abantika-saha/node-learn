0a4771d00d685fc6df4a45f48a7dc3a1c1132985
abantika-saha <65250567+abantika-saha@users.noreply.github.com>
2021-05-11 13:19:11 +0530

    Update appusingejs.js

--- appusingejs.js
+++ appusingejs.js
@@ -14,10 +14,15 @@ app.set('view engine', 'ejs');
 app.listen(3000); 
 
 app.get('/',(req,res)=>{
-    res.render('index'); //look inside the views folder and find index auto
+    const blogs=[
+        {title:'Talking points memo',snippet:'At some point during the disputed US election of 2000 - when Al Gore was famously defeated by a few hanging chads - Joshua Micah Marshall lost patience. Despite working as a magazine editor, Marshall chose to vent on the web. Eight years later Talking Points Memo and its three siblings draw in more than 400,000 viewers a day from their base in New York.'},
+        {title:'The Drudge Report',snippet:'The Report started life as an email gossip sheet, and then became a trashy webzine with negligible traffic. But thanks to the decision in 1998 to run a scurrilous rumour – untouched by mainstream media – about Bill Clinton and a White House intern named Monica Lewinsky, it became a national phenomenon. Recent scoops include Barack Obama dressed in tribal garb and the fact Prince Harry was serving in Afghanistan. Drudge is scorned by journalists and serious bloggers for his tabloid sensibilities, but his place in the media history books is guaranteed. And much though they hate him, the hacks all still check his front page – just in case he gets another president-nobbling scoop.'},
+        {title:'Beppe Grillo',snippet:'Among the most visited blogs in the world is that of Beppe Grillo, a popular Italian comedian and political commentator, long persona non grata on state TV, who is infuriated daily - especially by corruption and financial scandal in his country.'},
+    ];
+    res.render('index',{title: 'Home',blogs}); //look inside the views folder and find index auto
 });
 app.get('/about',(req,res)=>{
-    res.render('about');
+    res.render('about',{title: 'About'});
 });
 
 //redirecting
@@ -26,9 +31,9 @@ app.get('/about-redirect',(req,res)=>{
 })
 
 app.get('/blogs/create',(req,res)=>{
-    res.render('create');
+    res.render('create',{title: 'Create a new blog'});
 })
 
 app.use((req,res)=>{
-    res.status(404).render('404');
+    res.status(404).render('404',{title: '404'});
 })
