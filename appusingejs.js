9fc901aa8f264e93ecfb29f18f40656eba30bd82
abantika-saha <65250567+abantika-saha@users.noreply.github.com>
2021-05-12 01:43:51 +0530

    Update appusingejs.js

--- appusingejs.js
+++ appusingejs.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app=express();
@@ -12,6 +13,24 @@ app.set('view engine', 'ejs');
 
 //listen for requests
 app.listen(3000); 
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
+});
+
 
 app.get('/',(req,res)=>{
     const blogs=[
@@ -34,6 +53,6 @@ app.get('/blogs/create',(req,res)=>{
     res.render('create',{title: 'Create a new blog'});
 })
 
-app.use((req,res)=> {
+app.use((req,res)=>{
     res.status(404).render('404',{title: '404'});
 })
