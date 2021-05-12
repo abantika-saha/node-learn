baa456056ea9df5e0c6b347b9b4b41a14d9beb4c
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-05-12 12:30:14 +0530

    Update appusingmongoose.js

--- appusingmongoose.js
+++ appusingmongoose.js
@@ -105,3 +105,60 @@ app.get('/blogs/create',(req,res)=>{
 app.use((req,res)=>{
     res.status(404).render('404',{title: '404'});
 })
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+
+//express app
+const app=express();
+
+//connect to MongoDB
+const dbURI = "mongodb+srv://abantika-saha:27A64b528@@cluster0.82snl.mongodb.net/Cluster0";
+
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true})
+ .then((result)=>app.listen(3000))
+ .catch((err)=>console.log(err));
+
+//register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+
+app.get('/',(req,res)=>{
+    res.redirect('/blogs');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about',{title: 'About'});
+});
+
+//redirecting
+app.get('/about-redirect',(req,res)=>{
+    res.redirect('/about');
+})
+
+//blog routes
+app.get('/blogs',(req,res)=>{
+    Blog.find().sort({createdAt:-1}) //sorts newest first
+    .then((result)=>{
+        res.render('index',{title: 'All Blogs', blogs: result})
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title: 'Create a new blog'});
+})
+
+app.use((req,res)=>{
+    res.status(404).render('404',{title: '404'});
+})
+
