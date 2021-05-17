e353624fe4daf224a161b38fe4487f154e5a69b6
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-05-17 16:26:41 +0530

    Create blogRoutes.js

---
+++ routes/blogRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const blogController = require('../controllers/blogController'); //.. to extract it from first folder and then the seconf file.
+
+const router = express.Router();
+
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
+
+module.exports = router;
